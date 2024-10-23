0c60daff6398ec9ba5014306a74b00b625ca2bc8
atul583-dev <atul583b@gmail.com>
2024-10-23 09:52:52 +0530

    First Commit

---
+++ src/main/java/com/ai/voiceapp/VoiceappApplication.java
@@ -0,0 +1,13 @@
+package com.ai.voiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/voiceapp/VoiceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.voiceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
